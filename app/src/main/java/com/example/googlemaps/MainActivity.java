32c5056ce532d4020ef8a81b56e2b0d2d97e66f8
Adonis Alcivar <aalcivarn@uteq.edu.ec>
2024-01-30 11:24:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/googlemaps/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.googlemaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity
+    implements OnMapReadyCallback {
+    GoogleMap mapa;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mapa = googleMap;
+
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.getUiSettings().setZoomControlsEnabled(true);
+
+        //CameraUpdate camUpd1 =
+                //CameraUpdateFactory
+                        //.newLatLngZoom(new LatLng(49.26445, -123.23887), 25);
+        //mapa.moveCamera(camUpd1)
+
+        LatLng madrid = new LatLng(40.68925, -74.04453);
+        CameraPosition camPos = new CameraPosition.Builder()
+                .target(madrid)
+                .zoom(50)
+                .bearing(20) //noreste arriba
+                .tilt(20) //punto de vista de la cámara 70 grados
+                .build();
+        CameraUpdate camUpd3 =
+                CameraUpdateFactory.newCameraPosition(camPos);
+        mapa.animateCamera(camUpd3);
+
+    }
+}
